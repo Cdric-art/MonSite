a75af7fe8ecdf8d73907973bac9761040b741583
Evrard Cedric <cdric.art@gmail.com>
2020-06-07 18:04:47 +0200

    First Repo

---
+++ app.js
@@ -0,0 +1,31 @@
+// SCROLL ANIMATE
+
+const ratio = .2;
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
+    observer.observe(r)
+})
+
+// ZOOM PROJECT ANIMATE
+
+$('[class*="project-"]').hover(function() {
+    $(this).addClass('scaleof');
+  }, function() {
+      $(this).removeClass('scaleof')
+  });
